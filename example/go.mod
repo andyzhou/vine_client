a347e30135f143a55c39a50c7c2df42fc04e9093
Andy Zhou <zhirui1977@163.com>
2022-04-23 21:46:15 +0800

    add read example

--- example/go.mod
+++ example/go.mod
@@ -2,5 +2,4 @@ module github.com/andyzhou/vine_client/example
 
 go 1.14
 
-require github.com/andyzhou/vine_client v0.0.0-20220423115033-01267b3a61c5
-
+require github.com/andyzhou/vine_client v0.0.0-20220423133553-bc3df1644489
---
+++ example/read.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	vine "github.com/andyzhou/vine_client"
+	"github.com/andyzhou/vine_client/example/define"
+	"log"
+)
+
+const (
+	FileShortUrl = "Z90wVp"
+)
+
+func main() {
+	//init client
+	client := vine.NewClient()
+
+	//add master node
+	err := client.AddNodes(define.VineMasterNode)
+	if err != nil {
+		log.Printf("connect nodes failed, err:%v", err.Error())
+		return
+	}
+
+	//read file data by short url
+	fileData, err := client.ReadFile(FileShortUrl)
+	if err != nil {
+		log.Printf("read file failed, err:%v", err.Error())
+		return
+	}
+
+	log.Printf("read file succeed, data:%v", string(fileData))
+}
